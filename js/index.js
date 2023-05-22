9bd6467c61e1ecb9ef704e43bc53a0e550474376
Derrick Nyasio <dnyasio2014@gmail.com>
2023-05-22 21:58:24 +0300

    Local storage

---
+++ js/index.js
@@ -0,0 +1,60 @@
+let myLibrary = JSON.parse(localStorage.getItem("myLibrary")) || [
+
+    {
+        title: "Bleach",
+        author: "Tite Kubo",
+        pages: 678
+    },
+    {
+        title: "Harry Potter",
+        author: "JK Rowlings",
+        pages: 500
+    }
+
+];
+
+function Book(title, author, pages) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages
+}
+
+function addBookToLibrary () {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+
+    const newBook = new Book(title, author, pages)
+    console.log(newBook)
+    myLibrary.push(newBook)
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+    console.log(myLibrary)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // your code here
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        addBookToLibrary();
+        form.reset();
+    })
+});
+
+function renderBook(book) {
+    let li = document.createElement("li");
+    li.className = "li";
+    li.innerHTML = `
+    <h1>Book Title: ${book.title}</h1>
+    <p>Author: ${book.author}</p>
+    <p class="like">Number of pages: ${book.pages} pages</p>
+    `
+    document.querySelector("#books").appendChild(li)
+}
+function handleBooks (arr) {
+    arr.forEach((book) => {
+        renderBook(book)
+    });
+}
+
+handleBooks(myLibrary)
